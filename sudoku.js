8381a998ddcd5fb7a438b45cfccfd7d328bcfc66
Natalie Mall <nkmall@ucdavis.edu>
2020-05-02 00:49:29 -0700

    semi-accurate check if puzzle was solved (checks that rows and columns each contain the 9 unique digits, but does not check if each 3x3 square contains the 9 uniqe digits)

--- sudoku.js
+++ sudoku.js
@@ -3,6 +3,7 @@
 
 
   stringOfNums = '005910308009403060027500100030000201000820007006007004000080000640150700890000420'
+  // stringOfNums = '095127384138459672724836915851264739273981546946573821317692458489715263562348197' //solved one
   console.log(stringOfNums[2])
   const rows = Array.from(document.querySelectorAll('tr'));
   const matrix = rows.map(
@@ -68,6 +69,7 @@ window.onload = () => {
 
   NumberSizeSwitcher = false // True when it's a small number 
   containsBigNumber = false
+
   const rows = Array.from(document.querySelectorAll('tr'));
   const matrix = rows.map(
     r => Array.from(r.querySelectorAll('td'))
@@ -110,6 +112,7 @@ document.addEventListener("keydown", event => {
 
     if (matrix[active.row][active.col].classList.toString().includes("preFilled") === false) {
       console.log(matrix[active.row][active.col].classList)
+      typedNum = i.toString() 
 
 
       if (event.key === i.toString()) {  //i.e. if a 5 is pressed
@@ -119,15 +122,18 @@ document.addEventListener("keydown", event => {
           matrix[active.row][active.col].classList.add("smallNumber");
           // matrix[active.row][active.col].classList.add(className);
           if (containsBigNumber === true) {
-            matrix[active.row][active.col].innerHTML = ''; // delete the big number if it contains it
+            matrix[active.row][active.col].innerHTML = ''; // delete the big number if it contains it -- currently does not work
             containsBigNumber = false;
-          } else if (matrix[active.row][active.col].innerHTML.includes(i.toString()) === false){ //if small number is already included
+          } else if (matrix[active.row][active.col].innerHTML.includes(i.toString()) === false){ //if small number is NOT already included
             console.log('Small number was included here')
             matrix[active.row][active.col].innerHTML = matrix[active.row][active.col].innerHTML + i.toString(); //concat here
             // if lenght is 
 
             // matrix[active.row][active.col].innerHTML = matrix[active.row][active.col].innerHTML.replace(i.toString(), '')
             // matrix[active.row][active.col].innerHTML = 'TEST';
+          } else if (matrix[active.row][active.col].innerHTML.includes(i.toString()) === true) { //small num was already included
+            matrix[active.row][active.col].innerHTML.replace(i.toString(), 'TEST');
+            console.log('Already included', typedNum)
           }
           containsBigNumber = false;
         } else { //if mode is set to big number 
@@ -138,39 +144,17 @@ document.addEventListener("keydown", event => {
         }
 
 
-
-
-        // if (matrix[active.row][active.col].innerHTML === i.toString()) {
-        // // if (i === 1) {
-        // console.log('TEST');
-        // matrix[active.row][active.col].innerHTML = '';
-        // } else {
-        // matrix[active.row][active.col].innerHTML = i.toString();
-        // }
-        // if (NumberSizeSwitcher === true) {
-        //   className = ('small' + i.toString()).toString();
-        //   matrix[active.row][active.col].classList.add("smallNumber")
-        //   matrix[active.row][active.col].classList.add(className);
-        // } else {
-        //   matrix[active.row][active.col].classList.remove("smallNumber");
-        //   matrix[active.row][active.col].classList.remove(className)
-        // }
+        checkSolved()
       }
 
 
       if (event.code === 'Space') {  //toggles between pencil notes and normal fill-in
         console.log('Spacebar yay')
         if (NumberSizeSwitcher === true) {
-          // matrix[active.row][active.col].classList.remove("smallNumberNext"); //removes pencil marks upon second space click
-          // matrix[active.row][active.col].classList.remove("small1");
           NumberSizeSwitcher = false
         } else {
           NumberSizeSwitcher = true
-          // matrix[active.row][active.col].classList.add("smallNumberNext");  //switches to pencil marks
 
-
-          // }
-          // }
           }
       }
 
@@ -202,7 +186,95 @@ function updateHighlights() {
 updateHighlights();
 
 
+function checkSolved(){
+  rowsComplete = true;
+  columnsComplete = true; 
+  matrix.forEach(row => {
+    console.log(row);
 
+  })
+
+  for (i=0; i<9; i++){
+    eachRowNums = document.getElementById("sudokuSquare").rows[i].innerText;
+    if (eachRowNums.includes("1") === true && 
+    eachRowNums.includes("2") === true &&
+    eachRowNums.includes("3") === true &&
+    eachRowNums.includes("4") === true &&
+    eachRowNums.includes("5") === true &&
+    eachRowNums.includes("6") === true &&
+    eachRowNums.includes("7") === true &&
+    eachRowNums.includes("8") === true &&
+    eachRowNums.includes("9") === true ) {
+    console.log(i, '- row is finally complete!')
+  } else {
+    rowsComplete = false 
+  }
+
+  }
+
+
+
+  for (col=0; col<9; col++){
+    fullColumn = ''
+    for (i=0; i<9; i++){
+      eachColumnCell = matrix[i][col].innerText;
+      console.log(eachColumnCell);  //concat this value onto a string, then check that each string has all 9 numbers 
+      fullColumn = fullColumn + eachColumnCell; 
+    }
+
+    if (fullColumn.includes("1") === true && 
+      fullColumn.includes("2") === true &&
+      fullColumn.includes("3") === true &&
+      fullColumn.includes("4") === true &&
+      fullColumn.includes("5") === true &&
+      fullColumn.includes("6") === true &&
+      fullColumn.includes("7") === true &&
+      fullColumn.includes("8") === true &&
+      fullColumn.includes("9") === true ) {
+      console.log('this Column is finally complete!')
+    } else {
+      columnsComplete = false; 
+    }
+  }
+
+
+  if ((rowsComplete === true) && (columnsComplete === true)){
+    document.getElementById("sudokuSquare") 
+    
+    const allCells = Array.from(document.querySelectorAll('td'));
+    // if allCells
+    // var stylesInSquare = document.getElementById("sudokuSquare").classList;
+    var pencilStyle = document.getElementsByClassName("smallNumber");
+    if (pencilStyle.length) {
+      console.log('Still includes pencil marks')
+      var pencilMarks = true
+    }
+    // var stylesInSquare.style[cssProperty]
+    if (pencilMarks !== true){
+      console.log('I think you have completed the puzzle!')}
+  }
+
+
+}
+
+
+checkSolved();
+
+
+if (pencilMarks !== true) {
+
+  // <!DOCTYPE html>
+  // <html>
+  //     <head>
+  //         <title> Fireworks </title>
+  //         <link rel="stylesheet" href="https://cdn.rawgit.com/Legacy-K/Common/a1d9927e/fireworks/fireworks.css" />
+  //         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  //         <script src="https://cdn.rawgit.com/Legacy-K/Common/a1d9927e/fireworks/fireworks.js"></script>
+  //     </head>
+  //     <body></body>
+  // </html>
+
+}
 
 } // onload
 
